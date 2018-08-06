dc5aa62e93bab2066df971ddded8c97cb31115d0
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-06 12:58:29 +1000

    Refactor shop calculator into separate functions

--- cp1404practicals/prac_03/shop_calculator.py
+++ cp1404practicals/prac_03/shop_calculator.py
@@ -1,14 +1,30 @@
 
-number_of_items = int(input("Please enter the number of items: "))
-while number_of_items < 0:
-    print("Invalid number of items!")
+
+
+def main():
+    number_of_items = get_item_number()
+    total = 0
+    i, total = calculate_total(number_of_items, total)
+    if total > 100:
+        total = total * 0.9
+        print("The total price for " + str(i + 1) + " items is $" + "{:.2f}".format(total))
+    else:
+        print("The total price for " + str(i + 1) + " items is $" + "{:.2f}".format(total))
+
+
+def calculate_total(number_of_items, total):
+    for i in range(number_of_items):
+        price = float(input("Please enter the price of item " + str(i + 1) + ": "))
+        total = price + total
+    return i, total
+
+
+def get_item_number():
     number_of_items = int(input("Please enter the number of items: "))
-total = 0
-for i in range(number_of_items):
-    price = float(input("Please enter the price of item " + str(i + 1) + ": "))
-    total = price + total
-if total > 100:
-    total = total * 0.9
-    print("The total price for " + str(i + 1) + " items is $" + "{:.2f}".format(total))
-else:
-    print("The total price for " + str(i + 1) + " items is $" + "{:.2f}".format(total))
+    while number_of_items < 0:
+        print("Invalid number of items!")
+        number_of_items = int(input("Please enter the number of items: "))
+    return number_of_items
+
+
+main()
