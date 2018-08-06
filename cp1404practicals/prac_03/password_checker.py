8425b11be4625a71a0f0274fd99bbfb21368fff5
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-06 11:38:24 +1000

    Add password check program

---
+++ cp1404practicals/prac_03/password_checker.py
@@ -0,0 +1,10 @@
+MINIMUM_LENGTH = 6
+
+def main()
+    password = input("Enter a password with at least {} characters ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        password = input("Enter a password with at least {} characters ".format(MINIMUM_LENGTH))
+
+    print('*' * len(password))
+
+main()
