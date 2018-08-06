ac0af96ae4b7c5d164a7a1bf312dbfb277b34f42
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-06 14:29:11 +1000

    Refactor word generator to include a validate letter function

--- cp1404practicals/prac_03/word_generator.py
+++ cp1404practicals/prac_03/word_generator.py
@@ -4,13 +4,27 @@ WORD_FORMAT = "cv"
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-#word_format = input("Enter a word format, v for Vowel and c for Consonant. eg: ccvcvvc: ").lower()
-word_format = ''.join(random.choice(WORD_FORMAT) for x in range(8))
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
 
-print(word)
+def main():
+    word_format = input("Enter a word format, v for Vowel and c for Consonant. eg: ccvcvvc: ").lower()
+    while is_valid_format(word_format) == False:
+        word_format = input("Enter a word format, v for Vowel and c for Consonant. eg: ccvcvvc: ").lower()
+    else:
+        word = ''
+        word_format = ''.join(random.choice(WORD_FORMAT) for x in range(8))
+        for kind in word_format:
+            if kind == "c":
+                word += random.choice(CONSONANTS)
+            else:
+                word += random.choice(VOWELS)
+
+        print(word)
+
+def is_valid_format(word_format):
+    for char in word_format:
+        if char in WORD_FORMAT:
+            return True
+        else:
+            return False
+
+main()
