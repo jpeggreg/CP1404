63bdd35442aaa27f4e253971cad15e75ada5571b
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-06 13:26:42 +1000

    Add temperatures program for refactoring

---
+++ cp1404practicals/prac_03/temperatures.py
@@ -0,0 +1,19 @@
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("Celsius: "))
+        fahrenheit = celsius * 9.0 / 5 + 32
+        print("Result: {:.2f} F".format(fahrenheit))
+    elif choice == "F":
+        farenheit = float(input("Farenheit: "))
+        celsius = 5 / 9 * (farenheit - 32)
+        print("Result: {:.2f} F".format(celsius))
+    else:
+        print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+print("Thank you.")
