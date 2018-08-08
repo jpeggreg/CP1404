bd274377f039d7ae77159017fe996fea34e28092
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-08 14:18:19 +1000

    Add comments and tidy up gopher program code

--- cp1404practicals/prac_03/GPS.py
+++ cp1404practicals/prac_03/GPS.py
@@ -7,6 +7,7 @@ MINIMUM_DEATH_RANGE = 0.05  # 5%
 MAXIMUM_DEATH_RANGE = 0.25  # 25%
 YEARS = 10
 
+# Main simulates total population change over given number of years using random births and deaths
 def main():
     gopher_total = GOPHER_POPULATION
     print("Welcome to the Gopher Population Simulator!")
@@ -22,13 +23,11 @@ def main():
 # determines how many gohpers are born based on random total 10% to 20% of current total population
 def gopher_births(gopher_total):
     gophers_born = random.uniform(MINIMUM_BIRTH_RANGE * gopher_total, MAXIMUM_BIRTH_RANGE * gopher_total)
-    int(gophers_born)
     return gophers_born
 
 # determines how many gohpers died based on random total 5% to 25% of current total population
 def gopher_deaths(gopher_total):
     gophers_died = random.uniform(MINIMUM_DEATH_RANGE * gopher_total, MAXIMUM_DEATH_RANGE * gopher_total)
-    int(gophers_died)
     return gophers_died
 
 main()
