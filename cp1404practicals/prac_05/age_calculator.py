80aef9343c77ce23b57f75a5e6dc222eb78d8dec
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-08-21 14:14:15 +1000

    Add part 2 function for prac 5 practice work

--- cp1404practicals/prac_05/age_calculator.py
+++ cp1404practicals/prac_05/age_calculator.py
@@ -12,3 +12,12 @@ while len(name_age_dict) < PEOPLE:
 
 for key, value in name_age_dict.items():
     print(key, value)
+
+
+#function to take two parrallel lists as input parameters and returns dictionary where key from first list matches values from second list
+names = ["Jack", "Jill", "Harry"]
+dates_of_birth = [(12, 4, 1999), (1, 1, 2000), (27, 3, 1982)]
+
+names_dob_dict = dict(zip(names, dates_of_birth))
+print(names_dob_dict)
+
