8f954a054f1dad02d914ee1796d901a21b162c58
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-09-15 12:59:30 +1000

    Add guitar function to add new guitar and write to file. unsuccessful at writing in proper format and can't find examples

--- cp1404practicals/prac_08/guitars.py
+++ cp1404practicals/prac_08/guitars.py
@@ -12,8 +12,47 @@ def main():
         guitars.append(guitar)
     in_file.close()
 
+    guitars.sort()
+
     # loop through and display all languages (using their str method)
     for guitar in guitars:
         print(guitar)
+
+
+    # add new guitar to list
+    new_guitar = []
+    name = input("Name: ")
+    while name == "":
+        print("Input can not be blank")
+        name = input("Name: ")
+    else:
+        new_guitar.append(name)
+    year = input("Year: ")
+    while year == "":
+        print("Input can not be blank")
+        year = input("Year: ")
+    else:
+        new_guitar.append(year)
+    cost = 0
+    while cost == 0:
+        # validate int input for cost
+        try:
+            cost = float(input("Cost: "))
+            if cost <= 0:
+                print("Cost must be > 0")
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    else:
+        new_guitar.append(float(cost))
+    new_guitar = Guitars(name, year, cost)
+    guitars.append(new_guitar)
+
+    for guitar in guitars:
+        print(guitar)
+
+    # write new list to csv file
+    with open("myguitars.csv", "w") as guitar_file:
+        for guitar in guitars:
+            guitar_file.write(str(guitar) + " \n")
 
 main()
