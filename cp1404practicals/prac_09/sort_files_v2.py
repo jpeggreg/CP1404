8f4146043f61808d23de5b74b3f95db4c4ffca1a
Greg Taylor <gregory.taylor1@my.jcu.edu.au>
2018-09-26 11:14:03 +1000

    Success with Do from scratch exercise Prac_09 version 2

--- cp1404practicals/prac_09/sort_files_v2.py
+++ cp1404practicals/prac_09/sort_files_v2.py
@@ -11,10 +11,8 @@ from collections import defaultdict
 def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
-
     # Change to desired directory
     os.chdir('FilesToSort')
-
     # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
@@ -29,38 +27,33 @@ def main():
             ext_list.append(ext)
     print(ext_list)
 
-    # create list of category keys to associate with extensions based on user input
-    # test list without having user input ['Docs', 'Docs', 'Images', 'Images', 'Docs', 'Spreadsheets', 'Spreadsheets', 'Images']
+    # create list of category keys to associate with extensions based on user preference
+    # test functionality without needing user input: key_list = ['Docs', 'Docs', 'Images', 'Images', 'Docs', 'Spreadsheets', 'Spreadsheets', 'Images']
     key_list = []
     for ext in ext_list:
         new_key = input("What category would you like to sort {} files into?".format(ext))
         key_list.append(new_key)
     print(key_list)
 
-    # merge lists into dictionary grouping file extensions into each category
-    temp_dict = defaultdict(list)
+    # merge lists into dictionary grouping file extensions with chosen category
+    ext_dict = defaultdict(list)
     for i, ext in zip(key_list, ext_list):
-        temp_dict[i].append(ext)
-    ext_dict = dict(temp_dict)
-    print(ext_dict)
+        ext_dict[i].append(ext)
 
+    # make each directory from key_list based on user input
     for key in ext_dict:
         try:
             os.mkdir(key)
-            for value in ext_dict.values():
-                for filename in os.listdir('.'):
-                    if filename[filename.find('.'):].strip(".") == key[value]:
-                        shutil.move(filename, key)
         except:
             FileExistsError
             print("Folder already exists")
 
-
-    # Loop through each file in the (current) directory to ask user for file group category
-    #for filename in os.listdir('.'):
-     #   ext_name = filename[filename.find('.'):]
-      #  ext_dict[""] = input("What category would you like to sort {} files into?".format(ext_name))
-
+    # move each file to a directory based on input by user (ext_list)
+    for filename in os.listdir('.'):
+        for value in ext_dict.values():
+            if filename[filename.find('.'):].strip(".") in value:
+                key = list(ext_dict.keys())[list(ext_dict.values()).index(value)]
+                shutil.move(filename, key)
 
 
 main()
